8182715fefcb05ad388c662427fb555d7f15d74d
CAMILA ROMERO <camilaar@byui.edu>
2022-09-16 18:32:49 -0400

    l02

---
+++ demo.js
@@ -0,0 +1,34 @@
+const {MongoClient} = require('mongodb');
+
+async function main() {
+
+    const uri = "mongodb+srv://camilaar:camilaar24.@cluster0.a63dcom.mongodb.net/?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.array.forEach(db => {
+        console.log(`- ${db.name}`);
+    })
+ 
+
+}
+
+
