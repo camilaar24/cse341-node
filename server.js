4c5fa6e6dd3fcfa0f9308de084d0d816a9dced47
CAMILA ROMERO <camilaar@byui.edu>
2022-09-16 22:11:28 -0400

    server

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
 
-app.use('/', require('./routes')) 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+ 
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
