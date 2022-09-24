dc2c5b741744831a4065d8d7b3f4d89886841058
CAMILA ROMERO <camilaar@byui.edu>
2022-09-24 17:48:18 -0400

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const contact = require('./contacts');
 
-router.use('/contacts', require('./contacts'));
+router.use('/', require('./swagger'));
+router.use('/contacts', contact);
+router.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = "Hello world! Contacts API";
+    res.send(docData);
+  })
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json()).use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+
+.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
